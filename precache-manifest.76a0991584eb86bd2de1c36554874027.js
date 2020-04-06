f3c2de8ba6012c2142c839353d22c2c050bacc63
Jiackie <Jiaqili1023@gmail.com>
2020-04-06 16:53:10 -0700

    Updates

---
+++ precache-manifest.76a0991584eb86bd2de1c36554874027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3c738a9beaa59f45977025e577593c",
+    "url": "/netflix-my-list/index.html"
+  },
+  {
+    "revision": "cc76aebbeaf4af35adb9",
+    "url": "/netflix-my-list/static/js/2.29b607e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-my-list/static/js/2.29b607e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00fd7fbdbce752c62dd5",
+    "url": "/netflix-my-list/static/js/main.77844a21.chunk.js"
+  },
+  {
+    "revision": "3d7e29df43e5f65735eb",
+    "url": "/netflix-my-list/static/js/runtime-main.ebf0f025.js"
+  }
+]);
